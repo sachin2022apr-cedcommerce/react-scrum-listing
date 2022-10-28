64bef19deea12c056a5d5696d6aeaa0ecb2d8db7
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-10-28 16:43:38 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import './App.css';
 import '@shopify/polaris/build/esm/styles.css';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
-// import Dashboard from './Components/Dashboard';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-      </Routes> */}
-      <Home />
+      </Routes>
     </div>
   );
 }
